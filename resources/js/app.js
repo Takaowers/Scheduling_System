3f5d7297d2fb8d3ab62b01efcd5a45162d3a3310
Takaowers <salarialoej101@gmail.com>
2024-08-14 15:50:01 +0800

    New Update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-import './bootstrap';
+import { createApp } from 'vue';
+import ExampleComponent from './components/ExampleComponent.vue';
+
+const app = createApp({});
+app.component('example-component', ExampleComponent);
+app.mount('#app');
---
+++ resources/js/component.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h1>Hello, Vue.js!</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ExampleComponent',
+  };
+  </script>
+  
+  <style>
+  /* Add your styles here */
+  </style>
+  
