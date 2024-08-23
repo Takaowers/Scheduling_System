5c412b828edc808342afa427bbf89041bf9e9049
Federizo <windwall517@gmail.com>
2024-08-23 15:19:12 +0800

    Front End

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Scheduler from './components/Scheduler';
+import './App.css';
+import Toolbar from './components/Toolbar';
+const data = [
+    { start_date:'2020-06-10 6:00', end_date:'2020-06-10 8:00', text:'Event 1', id: 1 },
+    { start_date:'2020-06-13 10:00', end_date:'2020-06-13 18:00', text:'Event 2', id: 2 }
+];
+ 
+class App extends Component {
+  state = {
+    currentTimeFormatState: true
+};
+
+handleTimeFormatStateChange = (state) => {
+    this.setState({
+        currentTimeFormatState: state
+    });
+}
+render() {
+  const { currentTimeFormatState } = this.state;
+  return (
+      <div>
+          <div className="tool-bar">
+              <Toolbar
+                  timeFormatState={currentTimeFormatState}
+                  onTimeFormatStateChange={this.handleTimeFormatStateChange}
+              />
+          </div>
+          <div className='scheduler-container'>
+              <Scheduler
+                  events={data}
+                  timeFormatState={currentTimeFormatState}
+              />
+          </div>
+      </div>
+  );
+}
+ }
+ export default App;
